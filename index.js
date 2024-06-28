960b7798f01dcc49abd4b9ac967c5f3206439890
tslam223 <tslam223@gmail.com>
2024-06-28 20:12:48 +0100

    post-request-

---
+++ index.js
@@ -0,0 +1,31 @@
+function signUp(event) {
+    event.preventDefault();
+  const username = document.getElementById("text").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  fetch("https://deluxgem-2.onrender.com/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      email: email,
+      password: password,
+    }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      alert(res.message);
+      console.log(res);
+    })
+    .catch((error) =>{
+      console.log(error)
+      alert("Failed")
+    })
+}
+
+
+
+
+
